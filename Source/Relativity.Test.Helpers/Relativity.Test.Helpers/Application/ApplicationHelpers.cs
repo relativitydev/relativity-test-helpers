1f35ec2635046a6f8385d683930c309a1fe064ec
Priya Pande <ppande@kcura.com>
2017-07-13 11:43:29 -0500

    added import

---
+++ Source/Relativity.Test.Helpers/Relativity.Test.Helpers/Application/ApplicationHelpers.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using kCura.Relativity.Client;
+using System.Threading;
+using kCura.Relativity.Client.DTOs;
+using System.Linq;
+
+namespace Relativity.Test.Helpers.Application
+{
+	public class ApplicationHelpers
+	{
+		public static Int32 ImportApplication(IRSAPIClient client, Int32 workspaceId, bool forceFlag, string filePath, int appArtifactID = -1)
+		{
+			int artifactID = 0;
+			client.APIOptions.WorkspaceID = workspaceId; //set the target workspace of application to be imported.
+
+			// Create an application install request. 
+			// This list contains the ArtifactID for each Relativity Application that you want to install.
+			List<int> appsToOverride = new List<int>();
+
+			// Set the forceFlag to true. The forceFlag unlocks any applications in the workspace 
+			// that conflict with the application that you are loading. The applications must be unlocked 
+			// for the install operation to succeed.
+
+			AppInstallRequest appInstallRequest = new AppInstallRequest();
+
+			appInstallRequest.FullFilePath = filePath;
+			appInstallRequest.ForceFlag = forceFlag;
+			appInstallRequest.AppsToOverride.Add(appArtifactID);
+
+			try
+			{
+				ProcessOperationResult por = null;
+				por = client.InstallApplication(client.APIOptions, appInstallRequest);
+
+				if (por.Success)
+				{
+					while (client.GetProcessState(client.APIOptions, por.ProcessID).State == ProcessStateValue.Running)
+					{
+						Thread.Sleep(10);
+					}
+
+					client.GetProcessState(client.APIOptions, por.ProcessID);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Failed to import Application" + ex.Message);
+			}
+
+			kCura.Relativity.Client.DTOs.Query<kCura.Relativity.Client.DTOs.RelativityApplication> query = new kCura.Relativity.Client.DTOs.Query<kCura.Relativity.Client.DTOs.RelativityApplication>();
+			query.Fields.Add(new FieldValue(RelativityApplicationFieldNames.Name));
+			query.Condition = new kCura.Relativity.Client.TextCondition(RelativityApplicationFieldNames.Name, kCura.Relativity.Client.TextConditionEnum.EqualTo, "Admin Migration Utility");
+			kCura.Relativity.Client.DTOs.QueryResultSet<kCura.Relativity.Client.DTOs.RelativityApplication> queryResultSet = client.Repositories.RelativityApplication.Query(query);
+
+			if (queryResultSet != null)
+			{
+				artifactID = queryResultSet.Results.FirstOrDefault().Artifact.ArtifactID;
+			}
+
+			return artifactID;
+		}
+	}
+}
