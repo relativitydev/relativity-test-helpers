3c938e3352e97074ce00a3b545aa9b94ba2f9255
Carlos Torres <ctorres@kcura.com>
2018-04-10 11:39:19 -0500

    Update Fields helper to allow more types and code cleanup

--- Source/Relativity.Test.Helpers/Relativity.Test.Helpers/ArtifactHelpers/Fields.cs
+++ Source/Relativity.Test.Helpers/Relativity.Test.Helpers/ArtifactHelpers/Fields.cs
@@ -1,11 +1,12 @@
 ﻿using kCura.Relativity.Client;
 using Relativity.API;
+using Relativity.Test.Helpers.ArtifactHelpers.Request;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-
+using DTOs = kCura.Relativity.Client.DTOs;
 namespace Relativity.Test.Helpers.ArtifactHelpers
 {
     /// <summary>
@@ -15,325 +16,89 @@ namespace Relativity.Test.Helpers.ArtifactHelpers
     /// </summary>
     /// 
     public class Fields
-	{
-		public static Int32 GetFieldArtifactID(String fieldname, IDBContext workspaceDbContext)
-		{
-			string sqlquery = @"SELECT [ArtifactID] FROM [EDDSDBO].[Field] Where[DisplayName] like @fieldname";
-
-			var sqlParams = new List<SqlParameter>
-			{
-				new SqlParameter("@fieldname", SqlDbType.NVarChar) {Value = fieldname}
-			};
-
-			Int32 artifactTypeId = workspaceDbContext.ExecuteSqlStatementAsScalar<Int32>(sqlquery, sqlParams);
-
-			return artifactTypeId;
-		}
-
-		public static Int32 GetFieldCount(IDBContext workspaceDbContext, int fieldArtifactId)
-		{
-			string sqlquery = String.Format(@"select count(*) from [EDDSDBO].[ExtendedField] where ArtifactID = @fieldArtifactId");
-
-			var sqlParams = new List<SqlParameter>
-			{
-				new SqlParameter("@fieldArtifactId", SqlDbType.NVarChar) {Value = fieldArtifactId}
-			};
-
-			int fieldCount = workspaceDbContext.ExecuteSqlStatementAsScalar<Int32>(sqlquery, sqlParams);
-
-			return fieldCount;
-		}
-
-
-
-		public static Int32 CreateField_FixedLengthText(IRSAPIClient client, Int32 workspaceID)
-		{
-			Int32 fieldID = 0;
-
-			//Set the workspace ID
-			client.APIOptions.WorkspaceID = workspaceID;
-
-			//Read the object type to add fields to
-			//kCura.Relativity.Client.DTOs.ObjectType objectToAddFieldTo = client.Repositories.ObjectType.ReadSingle(objectTypeID);
-
-			//Create a Field DTO
-			kCura.Relativity.Client.DTOs.Field fieldDTO = new kCura.Relativity.Client.DTOs.Field();
-
-			//Set primary fields
-			//The name of the sample data is being set to a random string so that sample data can be debugged
-			//and never causes collisions. You can set this to any string that you want
-			fieldDTO.Name = string.Format("FixedLegthText{0}", Guid.NewGuid());
-			fieldDTO.ObjectType = new kCura.Relativity.Client.DTOs.ObjectType()
-			{
-				DescriptorArtifactTypeID = (int)ArtifactType.Document
-			};
-			fieldDTO.FieldTypeID = kCura.Relativity.Client.FieldType.FixedLengthText;
-
-			//Set secondary fields
-			fieldDTO.AllowHTML = false;
-			fieldDTO.AllowGroupBy = false;
-			fieldDTO.AllowPivot = false;
-			fieldDTO.AllowSortTally = false;
-			fieldDTO.IncludeInTextIndex = true;
-			fieldDTO.IsRequired = false;
-			fieldDTO.OpenToAssociations = false;
-			fieldDTO.Length = 255;
-			fieldDTO.Linked = false;
-			fieldDTO.Unicode = true;
-			fieldDTO.Width = "";
-			fieldDTO.Wrapping = true;
-			fieldDTO.IsRelational = false;
-
-			//Create the field
-			kCura.Relativity.Client.DTOs.WriteResultSet<kCura.Relativity.Client.DTOs.Field> resultSet =
-				client.Repositories.Field.Create(fieldDTO);
-			//Check for success
-			if (resultSet.Success)
-			{
-				fieldID = resultSet.Results.FirstOrDefault().Artifact.ArtifactID;
-
-				return fieldID;
-			}
-			else
-			{
-				throw new Exception("Field was not created");
-				return fieldID;
-			}
-		}
-
-
-		public static Int32 CreateField_LongText(IRSAPIClient client, int workspaceID)
-		{
-
-			Int32 fieldID = 0;
-
-			//Set the workspace ID
-			client.APIOptions.WorkspaceID = workspaceID;
-
-			//Create a Field DTO
-			kCura.Relativity.Client.DTOs.Field fieldDTO = new kCura.Relativity.Client.DTOs.Field();
-
-			//Set primary fields
-			//The name of the sample data is being set to a random string so that sample data can be debugged
-			//and never causes collisions. You can set this to any string that you want
-			fieldDTO.Name = string.Format("API Sample {0}", Guid.NewGuid());
-			fieldDTO.ObjectType = new kCura.Relativity.Client.DTOs.ObjectType()
-			{
-				DescriptorArtifactTypeID = (int)ArtifactType.Document
-			};
-			fieldDTO.FieldTypeID = kCura.Relativity.Client.FieldType.LongText;
-
-			// Set secondary fields
-			fieldDTO.AllowHTML = false;
-			fieldDTO.AllowGroupBy = false;
-			fieldDTO.AllowPivot = false;
-			fieldDTO.AllowSortTally = false;
-			fieldDTO.AvailableInViewer = false;
-			fieldDTO.IncludeInTextIndex = true;
-			fieldDTO.IsRequired = false;
-			fieldDTO.OpenToAssociations = false;
-			fieldDTO.Linked = false;
-			fieldDTO.Unicode = true;
-			fieldDTO.Width = "";
-			fieldDTO.Wrapping = true;
-
-			//Create the field
-			kCura.Relativity.Client.DTOs.WriteResultSet<kCura.Relativity.Client.DTOs.Field> resultSet = client.Repositories.Field.Create(fieldDTO);
-
-			//Check for success
-			if (resultSet.Success)
-			{
-				fieldID = resultSet.Results.FirstOrDefault().Artifact.ArtifactID;
-				return fieldID;
-			}
-			else
-			{
-				Console.WriteLine("Field was not created");
-				return fieldID;
-			}
-		}
-
-		public static Int32 CreateField_WholeNumber(IRSAPIClient client, int workspaceID)
-		{
-			Int32 fieldID = 0;
-
-			//Set the workspace ID
-			client.APIOptions.WorkspaceID = workspaceID;
-
-			//Create a Field DTO
-			kCura.Relativity.Client.DTOs.Field fieldDTO = new kCura.Relativity.Client.DTOs.Field();
-
-			//Set primary fields
-			//The name of the sample data is being set to a random string so that sample data can be debugged
-			//and never causes collisions. You can set this to any string that you want
-			fieldDTO.Name = string.Format("API Sample {0}", Guid.NewGuid());
-			fieldDTO.ObjectType = new kCura.Relativity.Client.DTOs.ObjectType()
-			{
-				DescriptorArtifactTypeID = (int)ArtifactType.Document
-			};
-			fieldDTO.FieldTypeID = kCura.Relativity.Client.FieldType.WholeNumber;
-
-			// Set secondary fields
-			fieldDTO.AllowGroupBy = false;
-			fieldDTO.AllowPivot = false;
-			fieldDTO.AllowSortTally = false;
-			fieldDTO.IsRequired = false;
-			fieldDTO.OpenToAssociations = false;
-			fieldDTO.Linked = false;
-			fieldDTO.Width = "";
-			fieldDTO.Wrapping = true;
-
-			//Create the field
-			kCura.Relativity.Client.DTOs.WriteResultSet<kCura.Relativity.Client.DTOs.Field> resultSet = client.Repositories.Field.Create(fieldDTO);
-
-			//Check for success
-			if (resultSet.Success)
-			{
-				fieldID = resultSet.Results.FirstOrDefault().Artifact.ArtifactID;
-				return fieldID;
-			}
-			else
-			{
-				Console.WriteLine("Field was not created");
-				return fieldID;
-			}
-		}
-
-		public static Int32 CreateField_YesNO(IRSAPIClient client, int workspaceID)
-		{
-			Int32 fieldID = 0;
-
-			//Set the workspace ID
-			client.APIOptions.WorkspaceID = workspaceID;
-
-			//Create a Field DTO
-			kCura.Relativity.Client.DTOs.Field fieldDTO = new kCura.Relativity.Client.DTOs.Field();
-
-			//Set primary fields
-			//The name of the sample data is being set to a random string so that sample data can be debugged
-			//and never causes collisions. You can set this to any string that you want
-			fieldDTO.Name = string.Format("API Sample {0}", Guid.NewGuid());
-			fieldDTO.ObjectType = new kCura.Relativity.Client.DTOs.ObjectType()
-			{
-				DescriptorArtifactTypeID = (int)ArtifactType.Document
-			};
-			fieldDTO.FieldTypeID = kCura.Relativity.Client.FieldType.YesNo;
-			fieldDTO.YesValue = "Yes";
-			fieldDTO.NoValue = "No";
-
-			// Set secondary fields
-			fieldDTO.AllowGroupBy = false;
-			fieldDTO.AllowPivot = false;
-			fieldDTO.AllowSortTally = false;
-			fieldDTO.IsRequired = false;
-			fieldDTO.OpenToAssociations = false;
-			fieldDTO.Linked = false;
-			fieldDTO.Width = "";
-			fieldDTO.Wrapping = true;
-
-			kCura.Relativity.Client.DTOs.WriteResultSet<kCura.Relativity.Client.DTOs.Field> resultSet = client.Repositories.Field.Create(fieldDTO);
-
-			//Check for success
-			if (resultSet.Success)
-			{
-				fieldID = resultSet.Results.FirstOrDefault().Artifact.ArtifactID;
-
-				return fieldID;
-			}
-			else
-			{
-				Console.WriteLine("Field was not created");
-				return fieldID;
-			}
-		}
-
-
-		public static Int32 CreateField_SingleChoice(IRSAPIClient client, int workspaceID)
-		{
-			Int32 fieldID = 0;
-
-			//Set the workspace ID
-			client.APIOptions.WorkspaceID = workspaceID;
-
-			//Create a Field DTO
-			kCura.Relativity.Client.DTOs.Field fieldDTO = new kCura.Relativity.Client.DTOs.Field();
-			fieldDTO.Name = string.Format("API Sample {0}", Guid.NewGuid());
-			fieldDTO.ObjectType = new kCura.Relativity.Client.DTOs.ObjectType()
-			{
-				DescriptorArtifactTypeID = (int)ArtifactType.Document
-			};
-			fieldDTO.FieldTypeID = kCura.Relativity.Client.FieldType.SingleChoice;
-
-			// Set secondary fields
-			fieldDTO.AllowGroupBy = false;
-			fieldDTO.AllowPivot = false;
-			fieldDTO.AllowSortTally = false;
-			fieldDTO.AvailableInFieldTree = false;
-			fieldDTO.IsRequired = false;
-			fieldDTO.OpenToAssociations = false;
-			fieldDTO.Linked = false;
-			fieldDTO.Unicode = true;
-			fieldDTO.Width = "";
-			fieldDTO.Wrapping = true;
-
-			//Create the field
-			kCura.Relativity.Client.DTOs.WriteResultSet<kCura.Relativity.Client.DTOs.Field> resultSet =
-				client.Repositories.Field.Create(fieldDTO);
-
-			//Check for success
-			if (resultSet.Success)
-			{
-				fieldID = resultSet.Results.FirstOrDefault().Artifact.ArtifactID;
-				return fieldID;
-			}
-			else
-			{
-				Console.WriteLine("Field was not created");
-				return fieldID;
-			}
-		}
-
-
-		public static Int32 CreateField_MultipleChoice(IRSAPIClient client, int workspaceID)
-		{
-			Int32 fieldID = 0;
-
-			//Set the workspace ID
-			client.APIOptions.WorkspaceID = workspaceID;
-
-			//Create a Field DTO
-			kCura.Relativity.Client.DTOs.Field fieldDTO = new kCura.Relativity.Client.DTOs.Field();
-
-			fieldDTO.Name = string.Format("API Sample {0}", Guid.NewGuid());
-			fieldDTO.ObjectType = new kCura.Relativity.Client.DTOs.ObjectType() { DescriptorArtifactTypeID = (int)ArtifactType.Document };
-			fieldDTO.FieldTypeID = kCura.Relativity.Client.FieldType.MultipleChoice;
-
-			// Set secondary fields
-			fieldDTO.AllowGroupBy = false;
-			fieldDTO.AllowPivot = false;
-			fieldDTO.AllowSortTally = false;
-			fieldDTO.AvailableInFieldTree = false;
-			fieldDTO.IsRequired = false;
-			fieldDTO.OpenToAssociations = false;
-			fieldDTO.Linked = false;
-			fieldDTO.Unicode = true;
-			fieldDTO.Width = "";
-			fieldDTO.Wrapping = true;
-
-			//Create the fields
-			kCura.Relativity.Client.DTOs.WriteResultSet<kCura.Relativity.Client.DTOs.Field> resultSet = client.Repositories.Field.Create(fieldDTO);
-
-			//Check for success
-			if (resultSet.Success)
-			{
-				fieldID = resultSet.Results.FirstOrDefault().Artifact.ArtifactID;
-				return fieldID;
-			}
-			else
-			{
-				Console.WriteLine("Field was not created");
-				return fieldID;
-			}
-		}
-	}
+    {
+        public static int GetFieldArtifactID(String fieldname, IDBContext workspaceDbContext)
+        {
+            string sqlquery = @"SELECT [ArtifactID] FROM [EDDSDBO].[Field] Where[DisplayName] like @fieldname";
+            var sqlParams = new List<SqlParameter>
+            {
+                new SqlParameter("@fieldname", SqlDbType.NVarChar) {Value = fieldname}
+            };
+            int artifactTypeId = workspaceDbContext.ExecuteSqlStatementAsScalar<int>(sqlquery, sqlParams);
+            return artifactTypeId;
+        }
+        public static int GetFieldCount(IDBContext workspaceDbContext, int fieldArtifactId)
+        {
+            string sqlquery = String.Format(@"select count(*) from [EDDSDBO].[ExtendedField] where ArtifactID = @fieldArtifactId");
+            var sqlParams = new List<SqlParameter>
+            {
+                new SqlParameter("@fieldArtifactId", SqlDbType.NVarChar) {Value = fieldArtifactId}
+            };
+            int fieldCount = workspaceDbContext.ExecuteSqlStatementAsScalar<int>(sqlquery, sqlParams);
+            return fieldCount;
+        }
+        public static int CreateField(IRSAPIClient client, FieldRequest request)
+        {
+            int fieldID = 0;
+            //Set the workspace ID
+            client.APIOptions.WorkspaceID = request.WorkspaceID;
+            //Create a Field DTO
+            var fieldDTO = new DTOs.Field();
+            //Set secondary fields
+            request.HydrateFieldDTO(fieldDTO);
+            //Create the field
+            var resultSet = client.Repositories.Field.Create(fieldDTO);
+            //Check for success
+            if (resultSet.Success)
+            {
+                fieldID = resultSet.Results.FirstOrDefault().Artifact.ArtifactID;
+            }
+            else
+            {
+                var innEx = resultSet.Results.Any() ? new Exception(resultSet.Results.First().Message) : null;
+                throw new Exception(resultSet.Message, innEx);
+            }
+            return fieldID;
+        }
+        public static int CreateField_Date(IRSAPIClient client, int workspaceID)
+        {
+            var fieldRequest = new FieldRequest(workspaceID, FieldType.Date);
+            return CreateField(client, fieldRequest);
+        }
+        public static int CreateField_User(IRSAPIClient client, int workspaceID)
+        {
+            var fieldRequest = new FieldRequest(workspaceID, FieldType.User);
+            return CreateField(client, fieldRequest);
+        }
+        public static int CreateField_FixedLengthText(IRSAPIClient client, int workspaceID)
+        {
+            var fieldRequest = new FieldRequest(workspaceID, FieldType.FixedLengthText);
+            return CreateField(client, fieldRequest);
+        }
+        public static int CreateField_LongText(IRSAPIClient client, int workspaceID)
+        {
+            var fieldRequest = new FieldRequest(workspaceID, FieldType.LongText);
+            return CreateField(client, fieldRequest);
+        }
+        public static int CreateField_WholeNumber(IRSAPIClient client, int workspaceID)
+        {
+            var fieldRequest = new FieldRequest(workspaceID, FieldType.WholeNumber);
+            return CreateField(client, fieldRequest);
+        }
+        public static int CreateField_YesNO(IRSAPIClient client, int workspaceID)
+        {
+            var fieldRequest = new FieldRequest(workspaceID, FieldType.YesNo);
+            return CreateField(client, fieldRequest);
+        }
+        public static int CreateField_SingleChoice(IRSAPIClient client, int workspaceID)
+        {
+            var fieldRequest = new FieldRequest(workspaceID, FieldType.SingleChoice);
+            return CreateField(client, fieldRequest);
+        }
+        public static int CreateField_MultipleChoice(IRSAPIClient client, int workspaceID)
+        {
+            var fieldRequest = new FieldRequest(workspaceID, FieldType.MultipleChoice);
+            return CreateField(client, fieldRequest);
+        }
+    }
 }
