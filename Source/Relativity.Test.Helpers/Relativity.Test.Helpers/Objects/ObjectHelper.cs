76c124246b31636e35aa8a5285766cabceeda09a
Alex Bilson <alex.bilson@relativity.com>
2019-02-13 13:59:56 -0600

    tries out the import api nuget

--- Source/Relativity.Test.Helpers/Relativity.Test.Helpers/Objects/ObjectHelper.cs
+++ Source/Relativity.Test.Helpers/Relativity.Test.Helpers/Objects/ObjectHelper.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Relativity.Test.Helpers.Application;
-using Relativity.Test.Helpers.Client;
-using Relativity.Test.Helpers.Document;
-using Relativity.Test.Helpers.Folder;
-using Relativity.Test.Helpers.Workspace;
-
-namespace Relativity.Test.Helpers.Objects
+﻿namespace Relativity.Test.Helpers.Objects
 {
 	public class ObjectHelper
 	{
-		public ApplicationHelper Application { get; set; }
-		public ClientHelper Client { get; set; }
-		public DocumentHelper Document { get; set; }
-		public FolderHelper Folder { get; set; }
-		public WorkspaceHelper Workspace { get; set; }
+		public Application.ApplicationHelper Application { get; set; }
+		public Client.ClientHelper Client { get; set; }
+		public Document.DocumentHelper Document { get; set; }
+		public Folder.FolderHelper Folder { get; set; }
+		public Workspace.WorkspaceHelper Workspace { get; set; }
 	}
 }
