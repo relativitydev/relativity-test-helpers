60d393015862b71d7f7d375173eeea0e5f94dffc
Thomas Lachin <thomas.lachin@relativity.com>
2019-07-25 11:02:58 -0500

    Added authentication manager implementation, using latest dbcontext nuget package, and added some basic integration tests for helpers for sanity checks

---
+++ Source/Relativity.Test.Helpers/Relativity.Test.Helpers.NUnit.Integration/TestEHHelperIntegrationTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Relativity.API;
+using Relativity.Test.Helpers.HelperClasses;
+using Relativity.Test.Helpers.SharedTestHelpers;
+using System;
+
+namespace Relativity.Test.Helpers.NUnit.Integration
+{
+	public class TestEHHelperIntegrationTests
+	{
+		private IEHHelper SuT;
+
+		[OneTimeSetUp]
+		public void SetUp()
+		{
+			SuT = new TestEHHelper(ConfigurationHelper.ADMIN_USERNAME, ConfigurationHelper.DEFAULT_PASSWORD);
+		}
+
+		[OneTimeTearDown]
+		public void TearDown()
+		{
+			SuT = null;
+		}
+
+		[Test]
+		public void GetAuthenticationManagerTest()
+		{
+			// Arrange
+			int userId = 0;
+
+			// Act
+			IAuthenticationMgr authenticationManager = SuT.GetAuthenticationManager();
+			userId = authenticationManager.UserInfo.ArtifactID;
+			Console.WriteLine($"GetAuthenticationManagerTest: userId = {userId}");
+
+			// Assert
+			Assert.IsTrue(userId > 0);
+		}
+	}
+}
