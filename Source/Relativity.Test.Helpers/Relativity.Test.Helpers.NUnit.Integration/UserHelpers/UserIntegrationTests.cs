1e17c79cb71f6585f1e495a585909f80adddfb30
Aaron Gilbert <aaron.gilbert@relativity.com>
2020-02-10 10:47:13 -0600

    Added Create/Delete User Integration Tests

---
+++ Source/Relativity.Test.Helpers/Relativity.Test.Helpers.NUnit.Integration/UserHelpers/UserIntegrationTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kCura.Relativity.Client;
+using NUnit.Framework;
+using Relativity.API;
+using Relativity.Test.Helpers.SharedTestHelpers;
+
+namespace Relativity.Test.Helpers.NUnit.Integration.UserHelpers
+{
+	[TestFixture]
+	public class UserIntegrationTests
+	{
+		private IHelper testHelper;
+		private IServicesMgr servicesMgr;
+		private IRSAPIClient rsapiClient;
+
+		[OneTimeSetUp]
+		public void SetUp()
+		{
+			testHelper = new TestHelper(ConfigurationHelper.ADMIN_USERNAME, ConfigurationHelper.DEFAULT_PASSWORD);
+			servicesMgr = testHelper.GetServicesManager();
+			rsapiClient = servicesMgr.CreateProxy<IRSAPIClient>(ExecutionIdentity.System);
+		}
+
+		[OneTimeTearDown]
+		public void TearDown()
+		{
+			testHelper = null;
+			servicesMgr = null;
+			rsapiClient = null;
+		}
+
+		[Test]
+		public void CreateUserAndDeleteUserTest()
+		{
+			// Act
+			int _userArtifactId = Relativity.Test.Helpers.UserHelpers.CreateUser.CreateNewUser(rsapiClient);
+			bool deleteUserResult = Relativity.Test.Helpers.UserHelpers.DeleteUser.Delete_User(rsapiClient, _userArtifactId);
+
+			// Assert
+			Assert.IsTrue(_userArtifactId > 0);
+			Assert.IsTrue(deleteUserResult);
+		}
+	}
+}
