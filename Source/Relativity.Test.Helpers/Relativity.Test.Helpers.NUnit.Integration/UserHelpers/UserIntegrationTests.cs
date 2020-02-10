a87515c3f4b76273dfa96eb2829f6105636c43b6
Aaron Gilbert <aaron.gilbert@relativity.com>
2020-02-10 11:28:46 -0600

    Added Cleanup before test

--- Source/Relativity.Test.Helpers/Relativity.Test.Helpers.NUnit.Integration/UserHelpers/UserIntegrationTests.cs
+++ Source/Relativity.Test.Helpers/Relativity.Test.Helpers.NUnit.Integration/UserHelpers/UserIntegrationTests.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using kCura.Relativity.Client;
+using kCura.Relativity.Client.DTOs;
 using NUnit.Framework;
 using Relativity.API;
 using Relativity.Test.Helpers.SharedTestHelpers;
+using User = kCura.Relativity.Client.DTOs.User;
 
 namespace Relativity.Test.Helpers.NUnit.Integration.UserHelpers
 {
@@ -23,6 +25,7 @@ namespace Relativity.Test.Helpers.NUnit.Integration.UserHelpers
 			testHelper = new TestHelper(ConfigurationHelper.ADMIN_USERNAME, ConfigurationHelper.DEFAULT_PASSWORD);
 			servicesMgr = testHelper.GetServicesManager();
 			rsapiClient = servicesMgr.CreateProxy<IRSAPIClient>(ExecutionIdentity.System);
+			CleanUpTempUsers();
 		}
 
 		[OneTimeTearDown]
@@ -43,6 +46,40 @@ namespace Relativity.Test.Helpers.NUnit.Integration.UserHelpers
 			// Assert
 			Assert.IsTrue(_userArtifactId > 0);
 			Assert.IsTrue(deleteUserResult);
+		}
+
+		public void CleanUpTempUsers()
+		{
+			rsapiClient.APIOptions.WorkspaceID = -1;
+			Condition userQueryCondition = new TextCondition(UserFieldNames.FirstName, TextConditionEnum.EqualTo, "Temp User");
+			Query<User> userQuery = new Query<User>(FieldValue.AllFields, userQueryCondition, new List<Sort>());
+			try
+			{
+				QueryResultSet<User> queryResultSet = rsapiClient.Repositories.User.Query(userQuery);
+				if (!queryResultSet.Success)
+				{
+					throw new Exception("Failed to Query for Temp Users");
+				}
+
+				if (queryResultSet.Results.Count > 0)
+				{
+					List<int> userArtifactIds = new List<int>();
+					foreach (Result<User> userResult in queryResultSet.Results)
+					{
+						userArtifactIds.Add(userResult.Artifact.ArtifactID);
+					}
+
+					WriteResultSet<User> deleteResultSet = rsapiClient.Repositories.User.Delete(userArtifactIds);
+					if (!deleteResultSet.Success)
+					{
+						throw new Exception("Failed to Delete Temp Users");
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("Error Cleaning Up Temp Users", ex);
+			}
 		}
 	}
 }
