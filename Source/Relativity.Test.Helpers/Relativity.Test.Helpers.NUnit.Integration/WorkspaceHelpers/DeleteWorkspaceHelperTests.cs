689b04b47c20a2f690d77dc0d646f61dd255d068
Jake Martin <jake.martin@relativity.com>
2020-02-17 14:06:54 -0600

    added delete workspace integration tests

---
+++ Source/Relativity.Test.Helpers/Relativity.Test.Helpers.NUnit.Integration/WorkspaceHelpers/DeleteWorkspaceHelperTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kCura.Relativity.Client;
+using NUnit.Framework;
+using Relativity.API;
+using Relativity.Test.Helpers.ArtifactHelpers;
+using Relativity.Test.Helpers.Exceptions;
+using Relativity.Test.Helpers.SharedTestHelpers;
+using Relativity.Test.Helpers.WorkspaceHelpers;
+
+namespace Relativity.Test.Helpers.NUnit.Integration.WorkspaceHelpers
+{
+	public class DeleteWorkspaceHelperTests
+	{
+		private IRSAPIClient _client;
+		private int _workspaceId;
+		private string _workspaceName = $"IntTest_{Guid.NewGuid()}";
+		private IServicesMgr _servicesManager;
+		private IHelper _testHelper;
+
+		[OneTimeSetUp]
+		public void SetUp()
+		{
+			_testHelper = new TestHelper(ConfigurationHelper.ADMIN_USERNAME, ConfigurationHelper.DEFAULT_PASSWORD);
+			_servicesManager = _testHelper.GetServicesManager();
+
+			_workspaceId = CreateWorkspace.CreateWorkspaceAsync(_workspaceName,
+				SharedTestHelpers.ConfigurationHelper.TEST_WORKSPACE_TEMPLATE_NAME, _servicesManager,
+				SharedTestHelpers.ConfigurationHelper.ADMIN_USERNAME, SharedTestHelpers.ConfigurationHelper.DEFAULT_PASSWORD).Result;
+		}
+
+		[OneTimeTearDown]
+		public void Teardown()
+		{
+			_testHelper = null;
+			_servicesManager = null;
+		}
+
+		[Test]
+		public void DeleteWorkspaceTest()
+		{
+			var success = DeleteWorkspace.DeleteTestWorkspace(_workspaceId, _servicesManager, ConfigurationHelper.ADMIN_USERNAME, ConfigurationHelper.DEFAULT_PASSWORD);
+
+			Assert.IsTrue(success);
+		}
+
+		[Test]
+		public void DeleteWorkspaceTest_Failure()
+		{
+			var success = DeleteWorkspace.DeleteTestWorkspace(0, _servicesManager, ConfigurationHelper.ADMIN_USERNAME, ConfigurationHelper.DEFAULT_PASSWORD);
+
+			Assert.IsFalse(success);
+		}
+	}
+}
