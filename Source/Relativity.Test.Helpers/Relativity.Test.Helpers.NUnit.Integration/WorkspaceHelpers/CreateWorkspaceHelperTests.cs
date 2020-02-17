633cb39e8ba3c78f3ba24fee84b32ea4c364c27f
Jake Martin <jake.martin@relativity.com>
2020-02-17 14:03:17 -0600

    added create workspace integration tests

--- Source/Relativity.Test.Helpers/Relativity.Test.Helpers.NUnit.Integration/WorkspaceHelpers/CreateWorkspaceHelperTests.cs
+++ Source/Relativity.Test.Helpers/Relativity.Test.Helpers.NUnit.Integration/WorkspaceHelpers/CreateWorkspaceHelperTests.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using kCura.Relativity.Client;
 using NUnit.Framework;
+using Relativity.API;
+using Relativity.Test.Helpers.ArtifactHelpers;
+using Relativity.Test.Helpers.Exceptions;
+using Relativity.Test.Helpers.SharedTestHelpers;
+using Relativity.Test.Helpers.WorkspaceHelpers;
 
 namespace Relativity.Test.Helpers.NUnit.Integration.WorkspaceHelpers
 {
@@ -13,29 +18,42 @@ namespace Relativity.Test.Helpers.NUnit.Integration.WorkspaceHelpers
 		private IRSAPIClient _client;
 		private int _workspaceId;
 		private string _workspaceName = $"IntTest_{Guid.NewGuid()}";
+		private IServicesMgr _servicesManager;
+		private IHelper _testHelper;
 
 		[OneTimeSetUp]
 		public void SetUp()
 		{
-
+			_testHelper = new TestHelper(ConfigurationHelper.ADMIN_USERNAME, ConfigurationHelper.DEFAULT_PASSWORD);
+			_servicesManager = _testHelper.GetServicesManager();
 		}
 
 		[OneTimeTearDown]
 		public void Teardown()
 		{
-
+			DeleteWorkspace.DeleteTestWorkspace(_workspaceId, _servicesManager, ConfigurationHelper.ADMIN_USERNAME,
+				ConfigurationHelper.DEFAULT_PASSWORD);
+			_testHelper = null;
+			_servicesManager = null;
 		}
 
 		[Test]
 		public void CreateWorkspaceTest()
 		{
+			_workspaceId = CreateWorkspace.CreateWorkspaceAsync(_workspaceName,
+				SharedTestHelpers.ConfigurationHelper.TEST_WORKSPACE_TEMPLATE_NAME, _servicesManager,
+				SharedTestHelpers.ConfigurationHelper.ADMIN_USERNAME, SharedTestHelpers.ConfigurationHelper.DEFAULT_PASSWORD).Result;
 
+			Assert.IsNotNull(_workspaceId);
 		}
 
 		[Test]
 		public void CreateWorkspaceTest_Failure()
 		{
+			var badTemplateName = "00template00";
 
+			Assert.Throws<AggregateException>(() => CreateWorkspace.CreateWorkspaceAsync(_workspaceName,
+				badTemplateName, _servicesManager, SharedTestHelpers.ConfigurationHelper.ADMIN_USERNAME, SharedTestHelpers.ConfigurationHelper.DEFAULT_PASSWORD).Wait());
 		}
 	}
 }
