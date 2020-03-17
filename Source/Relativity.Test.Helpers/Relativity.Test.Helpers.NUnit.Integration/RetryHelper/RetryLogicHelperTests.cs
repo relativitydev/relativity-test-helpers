e2cf15bb07661c54173e11d5d510d7c9c738517f
Thomas Lachin <thomas.lachin@relativity.com>
2020-03-17 15:51:30 -0500

    Polly retry logic interface and implementation

---
+++ Source/Relativity.Test.Helpers/Relativity.Test.Helpers.NUnit.Integration/RetryHelper/RetryLogicHelperTests.cs
@@ -0,0 +1,137 @@
+﻿using NUnit.Framework;
+using Relativity.Test.Helpers.Exceptions;
+using Relativity.Test.Helpers.RetryHelper;
+using System;
+using System.Threading.Tasks;
+
+namespace Relativity.Test.Helpers.NUnit.Integration.RetryHelper
+{
+	[TestFixture]
+	public class RetryLogicHelperTests
+	{
+		private RetryLogicHelper SuT;
+
+		[SetUp]
+		public void SetUp()
+		{
+			SuT = new RetryLogicHelper();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+
+		}
+
+		[TestCase(0, 0)]
+		[TestCase(2, 2)]
+		public void RetryFunction_Return_Valid(int numberOfRetries, int delayInSeconds)
+		{
+			// Arrange
+			// Act
+			int result = SuT.RetryFunction<int>(numberOfRetries, delayInSeconds, TestFunction);
+
+			// Assert
+			Assert.AreEqual(10, result);
+		}
+
+		[TestCase(-1, 2)]
+		[TestCase(2, -1)]
+		public void RetryFunction_Return_Invalid(int numberOfRetries, int delayInSeconds)
+		{
+			// Arrange
+			// Act
+			// Assert
+			Assert.Throws<TestHelpersException>(() => { SuT.RetryFunction<int>(numberOfRetries, delayInSeconds, TestFunction); });
+		}
+
+		[TestCase(0, 0)]
+		[TestCase(2, 2)]
+		public async Task RetryFunctionAsync_Return_Valid(int numberOfRetries, int delayInSeconds)
+		{
+			// Arrange
+			// Act
+			int result = await SuT.RetryFunctionAsync<int>(numberOfRetries, delayInSeconds, TestFunctionAsync);
+
+			// Assert
+			Assert.AreEqual(10, result);
+		}
+
+		[TestCase(-1, 2)]
+		[TestCase(2, -1)]
+		public async Task RetryFunctionAsync_Return_Invalid(int numberOfRetries, int delayInSeconds)
+		{
+			// Arrange
+			await Task.Yield();
+
+			// Act
+			// Assert
+			Assert.ThrowsAsync<TestHelpersException>(async () => { await SuT.RetryFunctionAsync<int>(numberOfRetries, delayInSeconds, TestFunctionAsync); });
+		}
+
+		[TestCase(0, 0)]
+		[TestCase(2, 2)]
+		public void RetryFunction_Void_Valid(int numberOfRetries, int delayInSeconds)
+		{
+			// Arrange
+			// Act
+			// Assert
+			Assert.DoesNotThrow(() => { SuT.RetryFunction(numberOfRetries, delayInSeconds, BasicVoidReturnFunction); });
+		}
+
+		[TestCase(-1, 2)]
+		[TestCase(2, -1)]
+		public void RetryFunction_Void_Invalid(int numberOfRetries, int delayInSeconds)
+		{
+			// Arrange
+			// Act
+			// Assert
+			Assert.Throws<TestHelpersException>(() => { SuT.RetryFunction(numberOfRetries, delayInSeconds, BasicVoidReturnFunction); });
+		}
+
+		[TestCase(0, 0)]
+		[TestCase(2, 2)]
+		public async Task RetryFunctionAsync_Void_Valid(int numberOfRetries, int delayInSeconds)
+		{
+			// Arrange
+			await Task.Yield();
+
+			// Act
+			// Assert
+			Assert.DoesNotThrowAsync(async () => { await SuT.RetryFunctionAsync(numberOfRetries, delayInSeconds, BasicVoidReturnFunctionAsync); });
+		}
+
+		[TestCase(0, 0)]
+		[TestCase(2, 2)]
+		public async Task RetryFunctionAsync_Void_Invalid(int numberOfRetries, int delayInSeconds)
+		{
+			// Arrange
+			await Task.Yield();
+
+			// Act
+			// Assert
+			Assert.DoesNotThrowAsync(async () => { await SuT.RetryFunctionAsync(numberOfRetries, delayInSeconds, BasicVoidReturnFunctionAsync); });
+		}
+
+		public int TestFunction()
+		{
+			return 10;
+		}
+
+		public async Task<int> TestFunctionAsync()
+		{
+			return await Task.FromResult(10);
+		}
+
+		public void BasicVoidReturnFunction()
+		{
+			Console.WriteLine("Void Return Function called");
+		}
+
+		public async Task BasicVoidReturnFunctionAsync()
+		{
+			await Task.Yield();
+			Console.WriteLine("Void Return Function called");
+		}
+	}
+}
---
+++ Source/Relativity.Test.Helpers/Relativity.Tests.Helpers.Tests.Unit/RetryHelper/RetryLogicHelperUnitTests.cs
@@ -0,0 +1,115 @@
+﻿using Moq;
+using NUnit.Framework;
+using Relativity.API;
+using Relativity.OAuth2Client.Exceptions;
+using Relativity.Test.Helpers.Exceptions;
+using Relativity.Test.Helpers.Kepler;
+using Relativity.Test.Helpers.RetryHelper;
+using System;
+using System.Threading.Tasks;
+
+namespace Relativity.Tests.Helpers.Tests.Unit.RetryHelper
+{
+	[TestFixture]
+	public class RetryLogicHelperUnitTests
+	{
+		private Mock<IOAuth2Helper> _mockOAuth2Helper;
+		private Mock<IHelper> _mockHelper;
+
+		private RetryLogicHelper SuT;
+
+		private const string OAuth2Id = "OAuth2Id";
+		private const string OAuth2Secret = "OAuth2Secret";
+		private readonly Guid AppGuid = new Guid("20378139-2b25-4c8e-9c31-4aba4cc30162");
+
+		[SetUp]
+		public void SetUp()
+		{
+			_mockOAuth2Helper = new Mock<IOAuth2Helper>();
+			_mockHelper = new Mock<IHelper>();
+
+			SuT = new RetryLogicHelper();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+
+		}
+
+		[TestCase(2, 2)]
+		public void RetryFunction_Return_Valid(int numberOfRetries, int delayInSeconds)
+		{
+			// Arrange
+			int workspaceId = 10;
+			int artifactId = 10;
+
+			_mockHelper
+				.SetupSequence(x => x.GetGuid(It.IsAny<int>(), It.IsAny<int>()))
+				.Throws(new TestHelpersException("Fake error, needs to retry"))
+				.Returns(AppGuid);
+
+			// Act
+			Guid result = SuT.RetryFunction<Guid>(numberOfRetries, delayInSeconds, () => _mockHelper.Object.GetGuid(workspaceId, artifactId));
+
+			// Assert
+			Assert.AreEqual(AppGuid, result);
+		}
+
+		[TestCase(2, 2)]
+		public async Task RetryFunctionAsync_Return_Valid(int numberOfRetries, int delayInSeconds)
+		{
+			// Arrange
+			Services.Security.Models.OAuth2Client oAuth2Client = new Services.Security.Models.OAuth2Client()
+			{
+				Id = OAuth2Id
+			};
+
+			_mockOAuth2Helper
+				.SetupSequence(x => x.CreateOAuth2ClientAsync(It.IsAny<string>(), It.IsAny<string>()))
+				.ThrowsAsync(new OAuth2ClientException("Fake error, needs to retry"))
+				.ReturnsAsync(oAuth2Client);
+
+			// Act
+			Services.Security.Models.OAuth2Client result = await SuT.RetryFunctionAsync(numberOfRetries, delayInSeconds, async () => await _mockOAuth2Helper.Object.CreateOAuth2ClientAsync(OAuth2Id, OAuth2Secret));
+
+			// Assert
+			Assert.AreEqual(OAuth2Id, oAuth2Client.Id);
+		}
+
+		[TestCase(2, 2)]
+		public void RetryFunction_Void_Valid(int numberOfRetries, int delayInSeconds)
+		{
+			// Arrange
+			Task completedTask = Task.CompletedTask;
+
+			_mockOAuth2Helper
+				.SetupSequence(x => x.DeleteOAuth2ClientAsync(It.IsAny<string>()))
+				.Throws(new OAuth2ClientException("Fake error, needs to retry"))
+				.Returns(completedTask);
+
+			// Act
+
+			// Assert
+			Assert.DoesNotThrow(() => SuT.RetryFunction(numberOfRetries, delayInSeconds, () => _mockOAuth2Helper.Object.DeleteOAuth2ClientAsync(OAuth2Id).Wait()));
+		}
+
+		[TestCase(2, 2)]
+		public async Task RetryFunctionAsync_Void_Valid(int numberOfRetries, int delayInSeconds)
+		{
+			// Arrange
+			await Task.Yield();
+			Task completedTask = Task.CompletedTask;
+
+			_mockOAuth2Helper
+				.SetupSequence(x => x.DeleteOAuth2ClientAsync(It.IsAny<string>()))
+				.Throws(new OAuth2ClientException("Fake error, needs to retry"))
+				.Returns(completedTask);
+
+			// Act
+
+			// Assert
+			Assert.DoesNotThrowAsync(async () => await SuT.RetryFunctionAsync(numberOfRetries, delayInSeconds, async () => await _mockOAuth2Helper.Object.DeleteOAuth2ClientAsync(OAuth2Id)));
+		}
+	}
+}
