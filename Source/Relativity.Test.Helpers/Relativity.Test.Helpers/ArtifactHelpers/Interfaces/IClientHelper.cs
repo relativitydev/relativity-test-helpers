e91952e07f8aa88e496aff848ab6ad1b8a8cc756
Jake Martin <jake.martin@relativity.com>
2021-02-19 12:57:08 -0600

    REL-518419 | added new ClientHelper class, interface, and ClientHelperTests class

---
+++ Source/Relativity.Test.Helpers/Relativity.Test.Helpers/ArtifactHelpers/Interfaces/IClientHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relativity.Test.Helpers.ArtifactHelpers.Interfaces
+{
+	public interface IClientHelper
+	{
+		int CreateClient(Services.ServiceProxy.ServiceFactory serviceFactory, string name);
+		void DeleteClient(Services.ServiceProxy.ServiceFactory serviceFactory, int artifactId);
+	}
+}
